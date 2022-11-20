0b1f751a0d5058d9acc4ab5de17b5e4e5e6eafa7
yz96 <68858113+yangzho12138@users.noreply.github.com>
2022-11-20 11:23:40 -0600

    sql compiler and VM

--- main.cpp
+++ main.cpp
@@ -3,11 +3,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct{
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+}MetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+}PrepareResult;
+
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+}StatementType;
+
+typedef struct {
     char* buffer; // the command line read in the terminal
     size_t buffer_length;
     ssize_t input_length;
 }InputBuffer;
+
+typedef struct {
+    StatementType type;
+}Statement;
 
 // return a pointer -> InputBuffer type variable
 InputBuffer* new_input_buffer(){
@@ -50,18 +69,71 @@ void close_input_buffer(InputBuffer* input_buffer){
     free(input_buffer);
 }
 
+MetaCommandResult do_meta_command(InputBuffer* input_buffer){
+    if(strcmp(input_buffer->buffer, ".exit") == 0){
+        exit(EXIT_SUCCESS);
+    }else{
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+// SQL Compiler
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
+    if(strncmp(input_buffer->buffer, "insert", 6) == 0 || strncmp(input_buffer->buffer, "INSERT", 6) == 0){
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }else if(strncmp(input_buffer->buffer, "select", 6) == 0 || strncmp(input_buffer->buffer, "SELECT", 6) == 0){
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+// Virtual Machine
+void execute_statement(Statement* statement){
+    switch (statement->type){
+        case (STATEMENT_INSERT):
+            // insert
+            printf("do insert\n");
+            break;
+        case (STATEMENT_SELECT):
+            // select
+            printf("do select\n");
+            break;
+    }
+}
+
+
 int main(int argc, char* argv[]){
 	InputBuffer* input_buffer = new_input_buffer(); // initial input_buffer
 	while(true){
 		print_prompt(); // print the prompt when the terminal call the corresponding command
 		read_input(input_buffer);
 
-		// exit command in the terminal
-		if(strcmp(input_buffer->buffer, ".exit") == 0){
-		    close_input_buffer(input_buffer);
-		    exit(EXIT_SUCCESS);
-		}else{
-		    printf("Unrecognized command '%s'. Please try again! \n", input_buffer->buffer);
+		// non-sql statements starts with '.', like .exit
+		if(input_buffer->buffer[0] == '.'){
+		    switch (do_meta_command(input_buffer)){
+		        case (META_COMMAND_SUCCESS):
+                    continue; // begin the next while loop
+		        case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                    continue;
+		    }
 		}
+
+		// sql statement
+		Statement statement;
+		switch (prepare_statement(input_buffer, &statement)){
+		    case (PREPARE_SUCCESS):
+		        break;
+		    case (PREPARE_UNRECOGNIZED_STATEMENT):
+                printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+                continue;
+		}
+
+		execute_statement(&statement);
+		printf("Executed successfully\n");
 	}
+
 }
